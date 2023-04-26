410783cfb31c4654be369beda6ab8bc92dce7431
penguip <99997237+penguip@users.noreply.github.com>
2023-04-26 16:45:16 -0500

    Update SALiquids.java

--- src/sentinels/content/SALiquids.java
+++ src/sentinels/content/SALiquids.java
@@ -1,1 +1,22 @@
-package
+package sentinels.content;
+
+import arc.graphics.*;
+import mindustry.content.*;
+import mindustry.type.*;
+
+public class SALiquids{
+    public static Liquid
+    pugneum;
+
+    public static void load() {
+        pugneum = new Liquid("pugneum"){{
+            temperature = 0f;
+            flammability = 0f;
+            explosiveness = .65f;
+            viscosity = 0.3f;
+            color = lightColor = Color.valueOf("F58859");
+            hideDetails = false;
+        }};
+    }
+}
+
