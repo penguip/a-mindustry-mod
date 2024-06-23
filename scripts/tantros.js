8604da97863e0486dba92f32abd73cf6673619ac
THE GREAT PAPYRUS <99997237+penguip@users.noreply.github.com>
2024-06-22 22:34:26 -0500

    Update tantros.js

--- scripts/tantros.js
+++ scripts/tantros.js
@@ -1,3 +1,3 @@
 Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
 Planets.tantros.localizedName = "Tantros"
-Planets.tantros.sectorSize = 3.5
+Planets.tantros.sectors = 4
